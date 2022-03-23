f101a0ced9ecd43154f6429bec75223edd923369
NullDev (Chris) <shadow@nulldev.org>
2022-03-23 21:34:25 +0100

    mapped orgas to ID's

--- src/classifier/orga.js
+++ src/classifier/orga.js
@@ -9,6 +9,13 @@ let path = require("path");
 
 let config = require("../utils/configHandler").getConfig();
 
+const ORGA_MAP = {
+    krebshilfe: 2,
+    kinderkrebsstiftung: 4,
+    dkms: 1,
+    dkfz: 3
+};
+
 /**
  * Classify object
  *
@@ -18,6 +25,6 @@ module.exports = async function(file){
     let result = cp.execSync(`bash ${path.resolve("./model/classify.sh")} ${path.resolve("./image_cache")}/${file}`).toString();
     let data = result.trim().split("\n")[0].trim().split(" ");
     return Number(data[data.length - 1].replace(")", "")) >= config.server.orga_confidence_threshold
-        ? data[0]
+        ? ORGA_MAP[data[0]]
         : null;
 };
