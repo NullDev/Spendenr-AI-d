f63a2f4a769bed2584690bf68915e414075f9a1f
NullDev (Chris) <shadow@nulldev.org>
2022-03-25 21:59:16 +0100

    improved regex

--- src/classifier/ocr.js
+++ src/classifier/ocr.js
@@ -14,9 +14,6 @@ let ocr = require("node-tesseract-ocr");
 // utils
 let log = require("../utils/logger");
 
-const lReg = /(eur|chf|$|€|euro|franken|dollar)(\s)*(\d+(?:(\.|\,)\d+)?)+/gi;
-const rReg = /(\d+(?:(\.|\,)\d+)?)+(\s)*(eur|chf|$|€|euro|franken|dollar)/gi;
-
 /**
  * Find amount
  *
@@ -26,11 +23,11 @@ module.exports = async function(file){
     try {
         let text = await ocr.recognize(fs.readFileSync(`${path.resolve("./image_cache")}/${file}`), { lang: "deu", psm: 3 });
 
-        let l = text.match(lReg);
-        let r = text.match(rReg);
+        let m = text.match(
+            /((eur|chf|\$|€|euro|franken|dollar)(\s)*)*(?<amount>(\d+(?:(\.|\,)\d+)?)+)((\s)*(eur|chf|\$|€|euro|franken|dollar))*/gi
+        ).filter(e => /(eur|chf|\$|€|euro|franken|dollar)/gi.test(e));
 
-        if (!l || !l.length) return (!r || !r.length) ? null : Number(r[0].replace(/[^0-9.,]/g, "").replace(",", "."));
-        return Number(l[0].replace(/[^0-9.,]/g, "").replace(",", "."));
+        return Number(m[0].trim().replace(/[^0-9.,]/g, "").replace(",", "."));
     }
     catch (e){
         log.error(e?.message);
