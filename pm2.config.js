7de509828e7d187cc1212bf0b005394450c920c7
NullDev (Shadow) <chris@nulldev.org>
2025-03-26 20:51:35 +0100

    update ecosystem

---
+++ pm2.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "spendenr-ai-d",
+    cwd: ".",
+    script: "src/app.js",
+    exec_mode: "cluster",
+    instances: 5,
+    repo: "https://github.com/NullDev/Spendenr-AI-d.git",
+    ref: "origin/ocr-only",
+    interpreter: "bun",
+    env: {
+        NODE_ENV: "production",
+        PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
+    },
+};
